69455c4e6e805463f977c5984875e245088db6e0
Piotr Macha <me@piotrmacha.pl>
2023-07-24 19:30:30 +0200

    Fix #1, fix volume at start: Replace zmusic instance in hook on CGameManager::Init() instead of Game_DefineExternals
    Signed-off-by: Piotr Macha <me@piotrmacha.pl>

--- plugin/src/Plugin.cpp
+++ plugin/src/Plugin.cpp
@@ -4,26 +4,36 @@
 #include <NH/BassOptions.h>
 #include <NH/Union.h>
 #include <NH/Bass.h>
+#include <Union/Hook.h>
 #include <ZenGin/zGothicAPI.h>
 
 #ifdef __G1
 #define GOTHIC_NAMESPACE Gothic_I_Classic
 #define ENGINE Engine_G1
+HOOKSPACE(Gothic_I_Classic, GetGameVersion() == ENGINE);
 #include "Plugin.hpp"
 #endif
 
 #ifdef __G2A
 #define GOTHIC_NAMESPACE Gothic_II_Addon
 #define ENGINE Engine_G2A
+HOOKSPACE(Gothic_II_Addon, GetGameVersion() == ENGINE);
 #include "Plugin.hpp"
 #endif
 
 #undef GOTHIC_NAMESPACE
 #undef ENGNE
+HOOKSPACE(Global, true);
 
 NH::Bass::BassOptions* NH::Bass::Options = new NH::Bass::BassOptions{};
 
 EXTERN_C_START
+__declspec(dllexport) void Game_Entry()
+{
+	NH::Bass::Options->LoggerLevelUnion = "DEBUG";
+	NH::Bass::Options->LoggerLevelZSpy = "DEBUG";
+}
+
 __declspec(dllexport) void Game_Loop() 
 {
 	auto* engine = NH::Bass::Engine::Initialize();
@@ -32,31 +42,12 @@ __declspec(dllexport) void Game_Loop()
 
 __declspec(dllexport) void Game_DefineExternals() 
 {
-	NH::Bass::Options->LoggerLevelUnion = "DEBUG";
-	NH::Bass::Options->LoggerLevelZSpy = "DEBUG";
-
 	if (GetGameVersion() == Engine_G1)
 	{
-		Gothic_I_Classic::ApplyOptions();
-		if (!Gothic_I_Classic::zoptions->Parm("ZNOMUSIC")) {
-			auto* bassEngine = NH::Bass::Engine::Initialize();
-			auto* directMusic = dynamic_cast<Gothic_I_Classic::zCMusicSys_DirectMusic*>(Gothic_I_Classic::zmusic);
-			directMusic->Stop();
-			Gothic_I_Classic::zmusic = new Gothic_I_Classic::CMusicSys_Bass(bassEngine, directMusic);
-			NH::Log::Info("ENTRY", "Set music system to CMusicSys_Bass");
-		}
 		Gothic_I_Classic::DefineExternals();
 	}
 	if (GetGameVersion() == Engine_G2A)
 	{
-		Gothic_II_Addon::ApplyOptions();
-		if (!Gothic_II_Addon::zoptions->Parm("ZNOMUSIC")) {
-			auto* bassEngine = NH::Bass::Engine::Initialize();
-			auto* directMusic = dynamic_cast<Gothic_II_Addon::zCMusicSys_DirectMusic*>(Gothic_II_Addon::zmusic);
-			directMusic->Stop();
-			Gothic_II_Addon::zmusic = new Gothic_II_Addon::CMusicSys_Bass(bassEngine, directMusic);
-			NH::Log::Info("ENTRY", "Set music system to CMusicSys_Bass");
-		}
 		Gothic_II_Addon::DefineExternals();
 	}
 }
--- plugin/src/Plugin.hpp
+++ plugin/src/Plugin.hpp
@@ -2,3 +2,4 @@
 #include <Gothic/Options.hpp>
 #include <Gothic/CMusicSys_Bass.hpp>
 #include <Gothic/Externals.hpp>
+#include <Gothic/Hooks.hpp>

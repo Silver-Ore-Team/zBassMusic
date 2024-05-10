01dafabf2509c60950a64f44ad4a53d3cf889486
Piotr Macha <me@piotrmacha.pl>
2024-05-10 06:04:46 +0200

    #5 Deactivate CGameManager::Init_Hook for non-supported versions

--- plugin/src/Gothic/Hooks.hpp
+++ plugin/src/Gothic/Hooks.hpp
@@ -2,9 +2,11 @@ namespace GOTHIC_NAMESPACE
 {
 	// G1:	0x004240C0 public: void __thiscall CGameManager::Init(struct HWND__ * &)
 	// G2A:	0x00424C70 public: void __thiscall CGameManager::Init(struct HWND__ * &)
-	auto Ivk_CGameManager_Init = Union::CreateHook(reinterpret_cast<void*>(zSwitch(0x004240C0, 0x00424C70)), &CGameManager::Init_Hook, Union::HookType::Hook_CallPatch);
+	auto Ivk_CGameManager_Init = Union::CreateHook(
+            GetGameVersion() == ENGINE ? reinterpret_cast<void*>(zSwitch(0x004240C0, 0x00424C70)) : 0,
+            &CGameManager::Init_Hook, Union::HookType::Hook_CallPatch);
 	inline void CGameManager::Init_Hook(HWND__*& hwnd)
-	{
+            {
         static NH::Logger* log = NH::CreateLogger("zBassMusic::CGameManager::Init_Hook");
 
 		if (GetGameVersion() != ENGINE)

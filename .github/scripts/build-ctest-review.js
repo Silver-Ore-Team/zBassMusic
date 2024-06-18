28a20817f385c1bc5c6eabb933d0af97d367c1dc
Piotr Macha <me@piotrmacha.pl>
2024-06-18 10:43:38 +0200

    ci: fix CTest bot

---
+++ .github/scripts/build-ctest-review.js
@@ -0,0 +1,24 @@
+module.exports = async ({github, context, status, log}) => {
+    const reviews = await github.rest.pulls.listReviews({
+        pull_number: context.pull.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+    });
+
+    reviews.find(r => r.user.login ==='github-actions' && r.state === 'COMMENTED').forEach(r => {
+        github.rest.pulls.dismissReview({
+            pull_number: context.pull.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            review_id: r.id
+        });
+    });
+
+    github.rest.pulls.createReview({
+        pull_number: context.pull.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: (status === 'success' ? '😍 Tests passed!' : '😨 Tests failed!') + '\n\n```\n' + log.replaceAll('%%%', "\n") + '\n```',
+        event: (status === 'success' ? 'APPROVE' : 'REQUEST_CHANGES')
+    });
+}

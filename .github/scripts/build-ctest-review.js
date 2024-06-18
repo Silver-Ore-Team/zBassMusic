2daff87a1b7175219f6d14b3c0c60edd3b86f575
Piotr Macha <me@piotrmacha.pl>
2024-06-18 10:51:56 +0200

    ci: fix CTest bot

--- .github/scripts/build-ctest-review.js
+++ .github/scripts/build-ctest-review.js
@@ -1,13 +1,13 @@
-module.exports = async ({github, context, status, log}) => {
+module.exports = async ({github, context, pull_number, status, log}) => {
     const reviews = await github.rest.pulls.listReviews({
-        pull_number: context.pull.number,
+        pull_number: pull_number,
         owner: context.repo.owner,
         repo: context.repo.repo,
     });
 
     reviews.find(r => r.user.login ==='github-actions' && r.state === 'COMMENTED').forEach(r => {
         github.rest.pulls.dismissReview({
-            pull_number: context.pull.number,
+            pull_number: pull_number,
             owner: context.repo.owner,
             repo: context.repo.repo,
             review_id: r.id
@@ -15,7 +15,7 @@ module.exports = async ({github, context, status, log}) => {
     });
 
     github.rest.pulls.createReview({
-        pull_number: context.pull.number,
+        pull_number: pull_number,
         owner: context.repo.owner,
         repo: context.repo.repo,
         body: (status === 'success' ? '😍 Tests passed!' : '😨 Tests failed!') + '\n\n```\n' + log.replaceAll('%%%', "\n") + '\n```',
